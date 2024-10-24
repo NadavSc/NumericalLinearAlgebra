1. fixed part2 LR approximation 2. fixed part2 error calculation 3. modified part1 condition number plot with proper legend location 4. added part1 rank fitted curves

part1/part1.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 from time import time
+from scipy.optimize import curve_fit
 from logger import set_logger, info
 matplotlib.use('TkAgg')
 
@@ -13,7 +14,7 @@ set_logger(log_path=os.path.join('../logger', 'log.txt'))
 
 def construct_A(lambda_, theta, delta, W, D):
     k = 2 * np.pi/lambda_
-    N = int(W/delta)
+    N = int(W/delta) + 1
 
     r_t = np.zeros((N, 2))
     r_r = np.zeros((N, 2))
@@ -47,8 +48,38 @@ def compute_SVD(A):
     return s_values, svd_time, ranks, condition_numbers
 
 
+def plot_fit_rank(threshold, save=False, log=False):
+    def rank_function(N, a, b):
+        """
+        Example fitting function: Rank = a * N^b
+        """
+        return a * np.power(N, b)
+
+    t_dics = {'τ=1e-2': 0, 'τ=1e-5': 1, 'τ=1e-8': 2}
+    for case, data in results.items():
+        ranks = [r[t_dics[threshold]] for r in data['ranks']]
+        N = np.array(data['N'])
+        plot_func = plt.loglog if log else plt.plot
+
+        plot_func(N, ranks, 'o', label=f'{case}')
+
+        popt, _ = curve_fit(rank_function, N, ranks)
+        a, b = popt  # Fitted parameters
+        N_fit = np.linspace(min(N), max(N), 500)
+        rank_fit = rank_function(N_fit, a, b)
+        plot_func(N_fit, rank_fit, '-', label=f'Fitted curve: Rank = {a:.2f} * N^{b:.2f}')
+    plt.xlabel('Number of Antennas (N)')
+    plt.ylabel('Rank')
+    plt.title(f'Matrix Rank - {threshold}')
+    plt.legend()
+    plt.grid(True, which="both", ls="-", alpha=0.5)
+    if save:
+        plt.savefig(f'part1_b_rank_{threshold[2:]}.png', dpi=300, bbox_inches='tight', pad_inches=0)
+
+
 if __name__ == '__main__':
     sections = ['b']  # Possible sections: 'a', 'b', 'c', 'd'
+    save = True
     for section in sections:
         if section == 'a':
             lambda_ = 1
@@ -94,14 +125,16 @@ if __name__ == '__main__':
             lambda_ = 1
             delta = lambda_ / 10
             theta = np.pi / 2
-            W_values = [4 * lambda_ * (2 ** i) for i in range(7)]
-            N_values = [int(w/delta) for w in W_values]
+            W_values = [4 * lambda_ * (2 ** i) for i in range(9)]
+            N_values = [int(w/delta)+1 for w in W_values]
+            case_to_msg = {'D = 4λ': 'D = 4lambda',
+                           'D = W': 'D = W',
+                           'D = W²/λ': 'D = W²/lambda'}
+            D_cases = {'D = 4λ': lambda W: 4 * lambda_,
+                       'D = W': lambda W: W,
+                       'D = W²/λ': lambda W: W ** 2 / lambda_
+                       }
 
-            D_cases = {
-                'D = 4λ': lambda W: 4 * lambda_,
-                'D = W': lambda W: W,
-                'D = W²/λ': lambda W: W ** 2 / lambda_
-            }
             if os.path.exists(r'results_b.pkl'):
                 with open(r'results_b.pkl', 'rb') as f:
                     results = pickle.load(f)
@@ -117,7 +150,7 @@ if __name__ == '__main__':
                                         delta=delta,
                                         W=W,
                                         D=D)
-                        info(f'Section B: A with {case}, W={W}, N={N} has been constructed')
+                        info(f'Section B: A with {case_to_msg[case]}, W={W}, N={N} has been constructed')
                         s_values, svd_time, ranks, condition_number = compute_SVD(A)
                         info(f'Section B: SVD has been calculated')
                         results[case]['N'].append(N)
@@ -131,7 +164,7 @@ if __name__ == '__main__':
                 plt.loglog(data['N'], data['time'], 'o', label=case, markersize=8)
 
             # Theoretical complexity O(N^3)
-            N_theory = np.logspace(1.5, 3, 100)
+            N_theory = np.logspace(1.5, 4, 100)
             time_theory = 1e-7 * N_theory ** 3  # Adjust the constant for visual fit
             plt.loglog(N_theory, time_theory, 'k--', label='O(N³) trend')
 
@@ -140,32 +173,44 @@ if __name__ == '__main__':
             plt.title('SVD Computation Complexity')
             plt.legend()
             plt.grid(True, which="both", ls="-", alpha=0.5)
-            plt.savefig('part1_b_svd_complexity.png', dpi=300, bbox_inches='tight', pad_inches=0)
+            if save:
+                plt.savefig('part1_b_svd_complexity.png', dpi=300, bbox_inches='tight', pad_inches=0)
             plt.show()
             plt.close()
 
             for case, data in results.items():
-                plt.semilogx(data['N'], [r[0] for r in data['ranks']], 'o', label=f'{case}, τ=1e-2')
-                plt.semilogx(data['N'], [r[1] for r in data['ranks']], 's', label=f'{case}, τ=1e-5')
-                plt.semilogx(data['N'], [r[2] for r in data['ranks']], '^', label=f'{case}, τ=1e-8')
+                plt.plot(data['N'], [r[0] for r in data['ranks']], 'o', label=f'{case}, τ=1e-2')
+                plt.plot(data['N'], [r[1] for r in data['ranks']], 's', label=f'{case}, τ=1e-5')
+                plt.plot(data['N'], [r[2] for r in data['ranks']], '^', label=f'{case}, τ=1e-8')
 
             plt.xlabel('Number of Antennas (N)')
             plt.ylabel('Rank')
             plt.title('Matrix Rank for Different Truncation Thresholds')
             plt.legend()
             plt.grid(True, which="both", ls="-", alpha=0.5)
-            plt.savefig('part1_b_rank.png', dpi=300, bbox_inches='tight', pad_inches=0)
+            if save:
+                plt.savefig('part1_b_rank_log.png', dpi=300, bbox_inches='tight', pad_inches=0)
             plt.show()
             plt.close()
 
+            plot_fit_rank(threshold='τ=1e-2', save=save, log=True)
+            plot_fit_rank(threshold='τ=1e-5', save=save, log=True)
+            plot_fit_rank(threshold='τ=1e-8', save=save, log=True)
+
+            plt.figure(figsize=(9, 6))
             for i, (case, data) in enumerate(results.items()):
-                plt.loglog(data['N'], data['condition'], 'o', label=['τ=1e-2', 'τ=1e-5', 'τ=1e-8'])
+                plt.loglog(data['N'], [c[0] for c in data['condition']], 'o', label=f'{case}, τ=1e-2')
+                plt.loglog(data['N'], [c[1] for c in data['condition']], 's', label=f'{case}, τ=1e-5')
+                plt.loglog(data['N'], [c[2] for c in data['condition']], '^', label=f'{case}, τ=1e-8')
                 plt.xlabel('Number of Antennas (N)')
                 plt.ylabel('Condition Number')
-                plt.title(f'Condition Number of Matrix A, {case}')
+                plt.title(f'Condition Number of Matrix A')
                 plt.legend()
                 plt.grid(True, which="both", ls="-", alpha=0.5)
-
-                plt.savefig(f'part1_b_condition_{i}.png', dpi=300, bbox_inches='tight', pad_inches=0)
-                plt.show()
-                plt.close()
+                plt.legend(loc='center left', bbox_to_anchor=(-0.5, 0.5), ncol=1,
+                           columnspacing=1.5, handletextpad=1.5, borderaxespad=0.5)
+                plt.subplots_adjust(left=0.3)
+            if save:
+                plt.savefig(f'part1_b_condition_all_log.png', dpi=300, bbox_inches='tight', pad_inches=0)
+            plt.show()
+            plt.close()

part2/part2.py
@@ -13,7 +13,7 @@ set_logger(log_path=os.path.join('../logger', 'log.txt'))
 
 def construct_A(lambda_, theta, delta, W, D):
     k = 2 * np.pi/lambda_
-    N = int(W/delta)
+    N = int(W/delta) + 1
 
     r_t = np.zeros((N, 2))
     r_r = np.zeros((N, 2))
@@ -49,17 +49,17 @@ def compute_SVD(A):
 
 def low_rank_approximation(A, tau):
     U, s, Vh = np.linalg.svd(A)
-    k = np.sum(s > tau) - 1
+    k = np.sum(s > tau)
     A_lr = U[:, :k] @ np.diag(s[:k]) @ Vh[:k, :]
-    return A_lr, k+1, s[:k]
+    return A_lr, k, s
 
 
 def compute_error(A, A_lr):
     return np.linalg.norm(A - A_lr) #/ np.linalg.norm(A)
 
 
-def theoretical_error(lr_s_values):
-    return np.sqrt(np.sum(lr_s_values**2))
+def theoretical_error(s_values, rank):
+    return np.sqrt(np.sum(s_values[rank:]**2))
 
 
 if __name__ == '__main__':
@@ -82,14 +82,14 @@ if __name__ == '__main__':
     times = []
 
     for tau in tau_values:
-        A_lr, rank, lr_s_values = low_rank_approximation(A, tau)
+        A_lr, rank, s_values = low_rank_approximation(A, tau)
 
         calc_time = np.mean(timeit.repeat(lambda: low_rank_approximation(A, tau), repeat=15, number=1))
         ranks.append(rank)
         errors.append(compute_error(A, A_lr))
-        theoretical_errors.append(theoretical_error(lr_s_values))
+        theoretical_errors.append(theoretical_error(s_values, rank))
         times.append(calc_time)
-        info(f'LR τ={tau} approximation has been calculated')
+        info(f'LR tau={tau} approximation has been calculated')
 
     plt.semilogx(tau_values, ranks, 'o')
     plt.xlabel('τ')
@@ -100,11 +100,12 @@ if __name__ == '__main__':
     plt.show()
     plt.close()
 
-    plt.loglog(tau_values, errors, 'o')
-    plt.axhline(y=theoretical_errors[0], color='k', linestyle='--')
+    plt.loglog(tau_values, errors, 'o', label='Error')
+    plt.loglog(tau_values, theoretical_errors, 'o', label='Theoretical error')
     plt.xlabel('τ')
     plt.ylabel('Relative Error (2-norm)')
     plt.title('Relative Error of LR Approximation')
+    plt.legend()
     plt.grid(True, which="both", ls="-", alpha=0.5)
     plt.savefig('lr_approx_error.png', dpi=300, bbox_inches='tight', pad_inches=0)
     plt.show()